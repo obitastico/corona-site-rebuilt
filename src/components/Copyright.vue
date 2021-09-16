7a63bd3416e54550d211dcb8ceae070f9db801b5
obitastico <obit-obit@web.de>
2021-09-16 22:02:20 +0200

    copyright responsive

--- src/components/Copyright.vue
+++ src/components/Copyright.vue
@@ -1,7 +1,9 @@
 <template>
-  <div>
-    <p class="copyright">Alle Angaben sind ohne Gewähr. Quelle:&nbsp;<a href="https://github.com/marlon360/rki-covid-api">corona-zahlen.org</a>&nbsp; © Website by {{ author }} 2021</p>
-  </div>
+    <p class="copyright">
+      Alle Angaben sind ohne Gewähr. <br class="small-br">
+      Quelle:&nbsp;<a href="https://github.com/marlon360/rki-covid-api">corona-zahlen.org</a><br class="small-br">
+      © Website by {{ author }} 2021
+    </p>
 </template>
 
 <script>
